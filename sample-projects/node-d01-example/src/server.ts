e0c38a23b1bc071991803e1989b4a4417f772c2e
Rusiru Gunaratne <info.rusiru@gmail.com>
2026-05-24 10:53:13 +0530

    Added movie database

--- sample-projects/node-d01-example/src/server.ts
+++ sample-projects/node-d01-example/src/server.ts
@@ -1,21 +1,36 @@
 import express, { type Request, type Response } from "express";
 
+interface Movie {
+    id: number;
+    title: string;
+    genre: string;
+    releaseYear: number;
+}
+
+const movieDatabase: Movie[] = [
+    { id: 1, title: "The Shawshank Redemption", genre: "Drama", releaseYear: 1994 },
+    { id: 2, title: "The Godfather", genre: "Crime", releaseYear: 1972 },
+    { id: 3, title: "The Dark Knight", genre: "Action", releaseYear: 2008 },
+    { id: 4, title: "Pulp Fiction", genre: "Crime", releaseYear: 1994 },
+    { id: 5, title: "Forrest Gump", genre: "Drama", releaseYear: 1994 },
+]
+
 const app = express();
 const PORT = 3000;
 
-// endpoint / -> response "Hello world from our express app"
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world from our express app");
-});
+// [GET] /api/movies - Get all movies
+app.get("/api/movies", (req: Request, res: Response) => {
+    const genreQuery = req.query.genre as string;
 
-app.get("/students", (req: Request, res: Response) => {
-  res.send("Some students data");
-});
+    if(genreQuery) {
+        const filteredMovies = movieDatabase.filter(
+            (movie) => movie.genre.toLocaleLowerCase() === genreQuery.toLocaleLowerCase()
+        );
+        res.json(filteredMovies);
+        return;
+    }
 
-// /students/12
-app.get("/students/:id", (req: Request, res: Response) => {
-  const studentId = req.params.id;
-  res.send(`Student data for student with id ${studentId}`);
+    res.json(movieDatabase);
 });
 
 app.listen(PORT, () => {
